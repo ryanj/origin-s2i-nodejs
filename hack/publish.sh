4c77dbc3ce41a6aa8234e36d390c10f45646321b
ryanj <ryan.jarvinen@gmail.com>
2016-08-17 11:42:29 -0700

    fixing test for using Docker credentials via env

--- hack/publish.sh
+++ hack/publish.sh
@@ -2,7 +2,7 @@
 
 BASE_IMAGES="${NAMESPACE}/${OS}-${ONBUILD_IMAGE_NAME} ${NAMESPACE}/${OS}-${BASE_IMAGE_NAME} ${NAMESPACE}/${OS}-${BASE_IMAGE_NAME}-candidate"
 
-if [ ! -z $DOCKER_USER && ! -z $DOCKER_PASS ]; then
+if [ ! -z $DOCKER_USER ] && [ ! -z $DOCKER_PASS ]; then
   echo "---> Authenticating to DockerHub..."
   docker login --username $DOCKER_USER --password $DOCKER_PASS
 fi
